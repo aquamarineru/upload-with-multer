e5cff612c006a5acdcb861eb8fbb69b16783aec3
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-10-28 12:40:00 +0200

    multer func, filename

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import multer from 'multer';
 
 const app = express()
 const port = process.env.PORT || 8080
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/images")
+    }, // cb function stores the images 1 argument = null,  2 argument: acutal destination Images = folder
+    filename: (req, file, cb) =>{ 
+        cb(null, Date.now() + '--' + (file.originalname)) //to create a unique filename
+    }
+})
+
+const upload = multer({storage: storage});
 
 app.listen(port, () => console.log(`The magic happens on port ${port}`));
